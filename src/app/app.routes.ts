ac2fbd061f29912a03eaaad5b46f53372f0dc839
andra <andrabaktiangjaya@gmail.com>
2026-02-04 16:29:52 +0800

    first init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'home',
+    loadComponent: () => import('./component/home/home').then((m) => m.Home)
+  },
+
+  {
+    path: 'product',
+    loadComponent: () => import('./component/product/product.component').then((m) => m.ProductComponent)
+  },
+
+  {
+    path: 'order',
+    loadComponent: () => import('./component/order/order').then((m) => m.Order)
+  },
+
+  {
+    path: 'receipt',
+    loadComponent: () => import('./component/receipt/receipt').then((m) => m.Receipt)
+  },
+
+];
---
+++ src/app/tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+};
